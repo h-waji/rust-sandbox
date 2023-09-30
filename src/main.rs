0ea02318c49f9b01ea35a2c6f6744f5bf2fdc806
h-waji <goo.hiro.w@gmail.com>
2023-09-30 19:38:01 +0900

    debug trait

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,26 @@
+#[derive(Debug)]
+struct Vocaloid {
+    name: String,
+    age: u8,
+    songs: Vec<String>,
+}
+impl Vocaloid {
+    fn new(name: String, age: u8, songs: Vec<String>) -> Self {
+        Self { name, age, songs }
+    }
+    fn info(&self) {
+        println!("vocaloid info: {:#?}", &self);
+    }
+}
+
 fn main() {
-    println!("Hello Rust!");
+    let songs = vec![
+        String::from("The Vampire"),
+        String::from("Cinderella"),
+        String::from("Ruma"),
+    ];
+    let miku = Vocaloid::new(String::from("Hatsune Miku"), 16, songs);
 
-    // 束縛：let
-    //    変数 -> 所有権：オブジェクト
-    let a = object;
-    let a: i32 = 0;
-
-    // 参照：仮の所有権を作成
-    let a = &object;
-
-    // 可変性
-    let a = object; // 変数a -> 原本 不変、所有権：オブジェクト
-    let mut a = object; // 変数a -> 原本 可変、所有権：オブジェクト
-    let a = &object; // 変数a -> 仮 不変、所有権：オブジェクト
-    let a = &mut object; // 変数a -> 仮 可変、所有権：オブジェクト
-
-    // 仮の所有権から原本の所有権は作れない
-    let a = object; // 変数a -> 原本 所有権：オブジェクト
-    let b = &a; // 変数b -> 仮 所有権
-    let c = b; // 変数c -> 仮 所有権
-    let d = &c; // 変数d -> 仮 所有権
+    println!("info: {:#?}", miku);
+    miku.info();
 }
