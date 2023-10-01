3d40740c95266c6662c3311b597dbdf6d8fbb5a5
h-waji <goo.hiro.w@gmail.com>
2023-10-01 19:26:25 +0900

    add unit test

--- src/vocaloid.rs
+++ src/vocaloid.rs
@@ -4,12 +4,22 @@ struct Vocaloid {
     age: u8,
     songs: Vec<String>,
 }
+
 impl Vocaloid {
     fn new(name: String, age: u8, songs: Vec<String>) -> Self {
         Self { name, age, songs }
     }
+
     fn info(&self) {
         println!("vocaloid info: {:#?}", &self);
+    }
+
+    fn is_adult(&self) -> bool {
+        self.age >= 18
+    }
+
+    fn say_hello(&self) -> String {
+        format!("Hello {}!", self.name)
     }
 }
 
@@ -20,7 +30,36 @@ pub fn run() {
         String::from("Ruma"),
     ];
     let miku = Vocaloid::new(String::from("Hatsune Miku"), 16, songs);
-
-    println!("info: {:#?}", miku);
     miku.info();
+    println!("{}", miku.is_adult());
+    println!("{}", miku.say_hello());
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_is_not_adult() {
+        let miku = Vocaloid::new(String::from("Hatsune Miku"), 16, vec![String::from("Ruma")]);
+        assert!(!miku.is_adult());
+    }
+
+    #[test]
+    fn test_is_adult() {
+        let kaito = Vocaloid::new(String::from("KAITO"), 26, vec![String::from("Ohed")]);
+        assert!(kaito.is_adult());
+    }
+
+    #[test]
+    fn test_say_hello_contains_name() {
+        let miku = Vocaloid::new(String::from("Hatsune Miku"), 16, vec![String::from("Ruma")]);
+        assert!(miku.say_hello().contains("Hatsune Miku"));
+    }
+
+    #[test]
+    fn test_say_hello_does_not_contain_different_name() {
+        let miku = Vocaloid::new(String::from("Hatsune Miku"), 16, vec![String::from("Ruma")]);
+        assert!(!miku.say_hello().contains("KAITO"));
+    }
 }
